2dbc849a61f8e936fab765fd54fe0fc7931a6ad1
Ryutaro Yokoo <yokokokokooo@iMac.local>
2026-03-28 18:17:06 +0900

    add OGP image and Twitter Card metadata

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,13 @@ export const metadata = {
     title: 'ポケモン 人気バトル',
     description: '全1025体のポケモンから「どっちが好き？」を選んで投票！',
     type: 'website',
+    images: [{ url: 'https://www.poke-vote.com/ogp.png', width: 1200, height: 630 }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ポケモン 人気バトル',
+    description: '全1025体のポケモンから「どっちが好き？」を選んで投票！',
+    images: ['https://www.poke-vote.com/ogp.png'],
   },
 };
 
